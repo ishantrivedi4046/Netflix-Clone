bff2a571273f52f05d06c6c0651e9cbcf6fbf339
ishu-yash <sheshtrivedi8046@gmail.com>
2024-01-19 14:40:35 +0530

    Initial Commit

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+const App: React.FC = () => {
+  return (
+    <div className=" w-full h-screen flex justify-center items-center text-2xl font-semibold">
+      Netflix Clone
+    </div>
+  )
+}
+
+export default App
